b3bc4cf75c8a34c4d306ad9584e3a90fa25ff9e5
Minu Kim <minu.kim2003@gmail.com>
2024-09-18 23:00:40 +0200

    Add project files.

---
+++ Sprint1/Controllers/HomeController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Sprint1.Controllers
+{
+    [Route("/")]
+    [ApiController]
+    public class HomeController : ControllerBase
+    {
+        [HttpGet]
+        public IActionResult Get()
+        {
+            return Ok("Hardcoded Response: Hello World!");
+        }
+    }
+}
